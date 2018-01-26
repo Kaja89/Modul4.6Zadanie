b583e0a43f28e086420760693fbc67345c35a42e
Kaja89 <kajaspiro@gmail.com>
2018-01-26 12:58:40 +0100

    simple calculator done

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+
+public class Calculator {
+	
+	public double add(double a, double b) {
+		double result = a + b;
+		System.out.println("Sum " + a + " + " + b + " equals: " + result);
+		return result;
+	}
+	
+	public double subtract(double a, double b) {
+		double result = a - b;
+		System.out.println("Sum " + a + " - " + b + " equals: " + result);
+		return result;
+	}
+}
---
+++ src/CalculatorApp.java
@@ -0,0 +1,10 @@
+
+public class CalculatorApp {
+
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		calc.add(2, 30);
+		calc.subtract(0, 4);
+	}
+
+}
